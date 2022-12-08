6bf135a89ac6566c95b58ca522fbb3a283886b25
Your Name <you@example.com>
2022-12-08 10:34:07 +0100

    first

---
+++ script.js
@@ -0,0 +1,263 @@
+const quizData = [
+    {
+        question: "1. Chirurg",
+        a: "je lékař, který léčí různá onemocnění nebo zranění operativně manuálním nebo instrumentálním ošetřením",
+        b: "podle určených technologických postupů vyrábí cukrářské výrobky, těsta, zpracovává čokoládu na čokoládové výrobky, vyrábí zmrzliny, zákusky, cukrovinky a dorty",
+        
+        correct: "a",
+    },
+    {
+      question: "2. Elektrikář",
+      a: "vykonává exekuce, jimiž je pověřen soudem na návrh oprávněného účastníka exekučního řízení",
+      b: "provádí veškeré činnosti týkající se slaboproudých nebo silnoproudých přístrojů a elektrického vedení",
+      
+      correct: "b",
+  },
+  {
+    question: "3. Fotograf",
+    a: "zhotovuje snímky v oblasti reprodukční a technické fotografie, fotografie architektury a krajiny a fotografie specializované pro různé technické a vědecké účely",
+    b: "poskytuje preventivní, diagnostickou a léčebnou péči ženám-pacientkám",
+    
+    correct: "a",
+},
+{
+  question: "4. Herec",
+  a: "podle dramatického textu interpretuje role tvůrčím způsobem – vypráví, přednáší v představeních v divadle, televizi, rozhlase, filmu nebo na jiných audiovizuálních nosičích",
+  b: "provádí hasičské a záchranné práce při požárech, mimořádných událostech a živelních pohromách",
+  
+  correct: "a",
+},{
+  question: "5. Kuchař",
+  a: "je ve své práci zodpovědný za sestavování jídelního lístku a vytvoření vhodné skladby pokrmů",
+  b: "se věnuje hlídání dětí, znamená kompletní péči o dítě",
+  
+  correct: "a",
+},
+{
+question: "6. Malíř",
+a: "provádí neléčebné, sportovní, pohotovostní, reflexní masáže",
+b: "je schopný aplikovat malby a nátěry na různé povrchy, a to jak v interiérech, tak i exteriérech",
+
+correct: "b",
+},{
+  question: "7. Manikérka a pedikérka",
+  a: "se stará o bezpečnost cestujících během celé přepravy, poskytuje jednotlivým pasažérům dostatečný komfort během letu a zajišťuje správné fungování všech zařízení",
+  b: "je pracovnice, která se zabývá péčí o ruce a nehty, nohy a nehty zákazníka",
+  
+  correct: "b",
+},
+{
+question: "8. Policista",
+a: "plní úkoly týkající se základního dohledu nad dopravou, odhalováním a vyřizováním přestupků, méně závažných trestných činů nebo závažných trestných činů",
+b: "řídí letadlo nebo vrtulník a zodpovídá za přesné a bezpečné provedení letu v souladu se všemi předpisy",
+
+correct: "a",
+},{
+  question: "9. Porodní asistentka",
+  a: "se stará o instalaci plynovodního vedení a spotřebičů, a to jak v interiérech, tak i exteriérech",
+  b: "je odborná pracovnice, která je vyškolena pro péči o ženu v průběhu normálního (nekomplikovaného) těhotenství, během porodu a šestinedělí",
+  
+  correct: "b",
+},
+{
+question: "10. Skladník",
+a: "řádně vykonává příslušné aktivity při skladování a zacházení se zbožím",
+b: "má za úkol vyhledávání obchodních příležitostí a oslovování potenciálních zákazníků",
+
+correct: "a",
+},{
+  question: "11. Právník",
+  a: "poskytuje právní služby, zastupuje klienty v občanskoprávních řízeních, zpracovává právní expertizy a provádí další odborné právnické činnosti.",
+  b: " odborný lékař, který se zabývá radiodiagnostikou, tzn. diagnostikou nemocí a chorob pomocí rentgenových metod",
+  
+  correct: "a",
+},
+{
+question: "12. Pokladní",
+a: "zajišťuje příjem a výdej peněz včetně poskytování pokladních služeb",
+b: "vyřizuje běžnou personální agendu v organizaci",
+
+correct: "a",
+},{
+  question: "13. Logoped",
+  a: "organizuje, řídí a kontroluje práce na svěřeném lesnickém úseku, provádí ochrannou službu, zajišťuje správu majetku a kontroluje myslivecké hospodaření na svěřeném úseku",
+  b: "se zabývá logopedickými, etopedickými a somatopedickými vyšetřeními a na jejich základě napravuje zjištěné poruchy",
+  
+  correct: "b",
+},
+{
+question: "14. Lékárník",
+a: "se stará o přípravu léčiv v lékárně",
+b: "je zodpovědný za management kvality ve společnosti",
+
+correct: "a",
+},{
+  question: "15. Kurýr",
+  a: " doručuje standardní a expresní zásilky, dále smluvně přepravuje osoby dle daných požadavků",
+  b: "zhotovuje a opravuje klempířské díly a výrobky, zejména ohýbáním, stříháním a dalším zpracováním plechu",
+  
+  correct: "a",
+},
+{
+question: "16. Novinář",
+a: "samostatně připravuje zprávy, články a reportáže pro tisk, rozhlas a televizi a zajišťuje zpravodajské, komentátorské a reportérské činnosti",
+b: "vytváří návrhy masek, maskování a líčení",
+
+correct: "a",
+},{
+  question: "17. Optik",
+  a: "má na starost kompletní řízení osvětlování na scéně",
+  b: "pracuje v oční optice a náplní jeho práce je seřizování a prodej korekčních očních pomůcek",
+  
+  correct: "b",
+},
+{
+question: "18. Programátor",
+a: "vytváří části aplikací nebo jejich kombinací staví větší celky. Kromě jejich tvorby spolupracuje i na jejich testování nebo ladění",
+b: " je lékař, který se zabývá nemocemi mozku, které se projevují v myšlení, emocích, vnímání nebo chování nemocných",
+
+correct: "b",
+},{
+  question: "19. Jeřábník",
+  a: "řídí zdvihací stroje a dopravuje jimi náklad na určené místo",
+  b: "provádí tvarování výrobků ze skloviny různými způsoby (rozfukováním a foukáním z volné ruky nebo do forem, roztáčením nalepováním, vrstvením skel)",
+  
+  correct: "a",
+},
+{
+question: "20. Geolog",
+a: "provádí geologický, geomorfologický a krajinně-ekologický výzkum a monitoring na určitém území",
+b: "je člověk, který se zabývá soustavným, racionálním a kritickým zkoumáním skutečnosti, světa a člověka, případně toho, co přesahuje (metafyzika)",
+
+correct: "a",
+},
+    
+  ];
+  
+  const quiz = document.getElementById('quiz')
+  const answerEls = document.querySelectorAll('.answer')
+  const questionEl = document.getElementById('question')
+  const result = document.getElementById('resultquestion')
+  const a_text = document.getElementById('a_text')
+  const b_text = document.getElementById('b_text')
+  
+  const submitBtn = document.getElementById('submit')
+  
+  let currentQuiz = 0
+  let score = 0
+  
+  loadQuiz()
+  
+  function loadQuiz() {
+    deselectAnswers()
+  
+    const currentQuizData = quizData[currentQuiz]
+  
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+   
+  }
+  
+  function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+  }
+  
+  function getSelected() {
+    let answer
+  
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+  
+    return answer
+  }
+  let answers= []; 
+  submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    
+    if(answer) { answers.push(answer);
+            console.log(answers);}
+  
+    if(answer) {
+        if(answer === quizData[currentQuiz].correct) {
+            score++;
+            
+            
+        }
+        
+        currentQuiz++;
+        
+  
+        if(currentQuiz < quizData.length) {
+            loadQuiz()
+        } else {
+            
+            
+  /* 
+  */
+           let results= quizData.map ((question,i) => 
+           
+           
+           ` 
+                <div class="${quizData[i].correct===answers[i]?
+                    "correct": "false" } quiz-header ">
+                <h2 id="question">${question.question}</h2>
+                <ul id="resultquestion"  >
+                  <li >
+                    <input type="radio" name="answer" id="a" class="answer">
+                    <label for="a" id="a_text">${question.a}</label>
+                  </li>
+        
+                  <li>
+                    <input type="radio" name="answer" id="b" class="answer">
+                    <label for="b" id="b_text">${question.b}</label>
+                  </li>
+        
+                  
+                  <li>
+                  <h4>Správná odpověď: ${question[quizData[i].correct]}</h4>
+                <h4>Vybral jsi: ${question[answers[i]]}</h4>
+  
+                
+                  </li>
+                  
+                </ul>
+              </div>`
+                
+                )
+  
+  //                 result.classList.add("correct")
+  // /* 
+                
+  
+                /* quizData.forEach ((question,i) =>
+                 quizData[i].correct===answers[i]?
+                  result.classList.add("correct")  : result.classList.add("false") 
+                 ) */
+            
+         
+  
+  
+            quiz.innerHTML = `
+                <h2>Získal jsi ${score} bodů z ${quizData.length}.</h2>
+               
+  
+                
+                
+               
+    
+                ${results}
+  
+  
+                
+  
+                <button onclick="location.reload()">Znovu zkusit</button>
+            `
+        
+        
+        }
+    }
+  })
